5289dd0dfaafad2085765c2357b5d087a98b7f11
unknown <Eugene.Yuriev@SCM-Yuriev.omegalliance.com>
2015-10-08 10:47:49 +0300

    Эксперименты с RAM

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,13 +10,45 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            /*
             Point p1 = new Point(2,5,'*');
             p1.Draw();
 
             Point p2 = new Point(4, 2, '*');
             p2.Draw();
-                        
+            */
+
+            int x;
+            x = 1;
+            Console.WriteLine(x);
+            func1(x);
+            Console.WriteLine(x);
+
+
+            Point p1 = new Point(1, 1, '*');
+            Point p2 = new Point();
+
+            //p2 = p1;
+            Copy(p2,p1);
+
+            p2.x = 2;
+            p2.y = 2;
+
+            Console.WriteLine("p1.x=" + p1.x + ", p1.y=" + p1.y);
+
             Console.ReadLine();
+        }
+
+        static void Copy(Point p1, Point p2)
+        {
+            p1.x = p2.x;
+            p1.y = p2.y;
+        }
+
+        static void func1(int x)
+        {
+            x = 2;
+            Console.WriteLine(x);
         }
     }
 }
