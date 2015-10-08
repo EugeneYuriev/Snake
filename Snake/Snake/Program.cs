932a46b210ef04863146be9b373156a962f254bb
unknown <Eugene.Yuriev@SCM-Yuriev.omegalliance.com>
2015-10-08 09:54:07 +0300

    Добавили процедуру для вывода точки на экран в заданной позиции

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,24 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            int x1 = 8;
+            int y1 = 5;
+            char sym1 = '*';
+
+            Draw(x1, y1, sym1);
+
+            int x2 = 4;
+            int y2 = 7;
+            char sym2 = '#';
+
+            Draw(x2, y2, sym2);
+
             Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char sym) {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
